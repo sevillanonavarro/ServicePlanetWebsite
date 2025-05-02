14a44644d9ce868f17dd1be684be84a62a29f2a3
sevillanonavarro <sevillanonavarro@serviceplanet.nl>
2025-05-02 16:33:07 +0200

    We need to fix the blue background because is not responsive

--- ScriptsHome/Menu.js
+++ ScriptsHome/Menu.js
@@ -5,3 +5,19 @@ AOS.init();
     menuToggle.addEventListener('click', () => {
       menuToggle.classList.toggle('open');
     });
+
+
+    function adjustBackgroundShapeHeight() {
+      const mainSection = document.querySelector('.section-wrapper');
+      const shape = document.querySelector('.background-shape');
+    
+      // Añade margen extra para asegurar que el recorte cubra todo
+      const extra = 100; // puedes ajustar este valor según necesidad
+      shape.style.height = (mainSection.offsetHeight + extra) + 'px';
+    }
+    
+    window.addEventListener('load', adjustBackgroundShapeHeight);
+    window.addEventListener('resize', adjustBackgroundShapeHeight);
+    
+
+    
