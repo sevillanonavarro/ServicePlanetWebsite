92bf2cef4ec9debb6480f00b0cd1455228bc1054
sevillanonavarro <sevillanonavarro@serviceplanet.nl>
2025-05-05 16:49:59 +0200

    Certification no JS solved

--- ScriptsHome/Certs.js
+++ ScriptsHome/Certs.js
@@ -1,19 +1,19 @@
-const sliderTrack = document.getElementById("awardSliderTrack");
+const sliderTrackCert = document.getElementById("awardSliderTrack");
 const prevArrow = document.getElementById("awardSliderPrev");
 const nextArrow = document.getElementById("awardSliderNext");
 
-const totalImages = sliderTrack.children.length;
+const totalImages = sliderTrackCert.children.length;
 const imagesVisible = 5;
 const imageGap = 20;
 let currentIndex = 0;
 
 const moveSlider = () => {
-  const singleImage = sliderTrack.querySelector("img");
+  const singleImage = sliderTrackCert.querySelector("img");
   const imageTotalWidth = singleImage.offsetWidth + imageGap;
   const maxIndex = totalImages - imagesVisible;
   if (currentIndex < 0) currentIndex = maxIndex;
   if (currentIndex > maxIndex) currentIndex = 0;
-  sliderTrack.style.transform = `translateX(-${currentIndex * imageTotalWidth}px)`;
+  sliderTrackCert.style.transform = `translateX(-${currentIndex * imageTotalWidth}px)`;
 };
 
 prevArrow.addEventListener("click", () => {
